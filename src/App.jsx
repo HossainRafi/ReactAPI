5358f584760def357fb725d0dd593da14ed0798d
HossainRafi <hossainrafi71@gmail.com>
2023-06-11 20:47:22 +0600

    heading

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 function App() {
-  return <div></div>;
+  return (
+    <div className="min-h-screen w-full bg-gradient-to-b from-gray-900 to-gray-600 py-10">
+      <h1 className="text-center text-3xl sm:text-4xl md:text-6xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-indigo-200 via-red-300 to-yellow-200">
+        Game Of Thrones
+      </h1>
+    </div>
+  );
 }
 
 export default App;
