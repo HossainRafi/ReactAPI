99ef448a47240e6eff7aec356fcc3b093c6b9613
HossainRafi <hossainrafi71@gmail.com>
2023-06-11 21:25:26 +0600

    get data from api

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,19 @@
-
+import { useEffect, useState } from "react";
 
 function Main() {
-    
-  return (
-    <div>main</div>
-  )
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("https://thronesapi.com/api/v2/Characters");
+      const results = await res.json();
+      setData(results);
+    };
+
+    fetchData();
+  }, []);
+  console.log(data);
+  return <div>main</div>;
 }
 
-export default Main
+export default Main;
