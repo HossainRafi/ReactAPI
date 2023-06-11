6afb38e1d2e2d08be18e8730f613c7880e42d60a
HossainRafi <hossainrafi71@gmail.com>
2023-06-11 21:53:57 +0600

    img design

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,26 @@
-
-
-function Card() {
-  return <div className="h-72 w-full">Card</div>;
+function Card({ url, title, family, firstName, lastName }) {
+  return (
+    <div
+      className="group h-72 w-full cursor-pointer"
+      style={{
+        perspective: "1000px",
+      }}
+    >
+      <div
+        style={{
+          transformStyle: "preserve-3d",
+        }}
+        className="relative h-full w-full rounded-xl shadow-xl transition-all duration-500 group-hover:[transform:rotateY(180deg)]"
+      >
+        <div className="absolute inset-0">
+          <img
+            src={url}
+            className="h-full w-full rounded-xl object-cover shadow-lg shadow-white/30"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
 import Card from "./Card";
+import { useEffect, useState } from "react";
 
 function Main() {
   const [data, setData] = useState([]);
